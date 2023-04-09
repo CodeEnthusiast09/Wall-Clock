d1cb78fa9e02e6a44c1dfcc3586af19bf1d929ed
OBrien-T <obrienadedapo15@gmail.com>
2023-04-09 01:22:00 +0100

    correction

--- script.js
+++ script.js
@@ -9,12 +9,12 @@ function setDate() {
     let secondsDegree = (seconds / 60) * 360 + 90;
     secondHand.style.transform = `rotate(${secondsDegree}deg)`;
 
-    var mins = now.getMinutes();
-    var minsDegrees = (mins / 60) * 360 + (seconds / 60) * 6 + 90;
+    let mins = now.getMinutes();
+    let minsDegrees = (mins / 60) * 360 + (seconds / 60) * 6 + 90;
     minuteHand.style.transform = `rotate(${minsDegrees}deg)`;
 
-    var hour = now.getHours();
-    var hourDegrees = (hour / 12) * 360 + (mins / 60) * 30 + 90;
+    let hour = now.getHours();
+    let hourDegrees = (hour / 12) * 360 + (mins / 60) * 30 + 90;
     hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 
 }
